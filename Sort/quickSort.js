e76157ce2f38500aee33cf3f938c0add6523df8f
bu <86216382+hindong@users.noreply.github.com>
2022-12-26 23:58:30 +0900

    Create quickSort.js

---
+++ Sort/quickSort.js
@@ -0,0 +1,65 @@
+function swap(arr, i, j) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+function partition(arr, low, high) {
+  
+    // pivot
+    let pivot = arr[high];
+  
+    // Index of smaller element and
+    // indicates the right position
+    // of pivot found so far
+    let i = (low - 1);
+  
+    for (let j = low; j <= high - 1; j++) {
+  
+        // If current element is smaller 
+        // than the pivot
+        if (arr[j] < pivot) {
+  
+            // Increment index of 
+            // smaller element
+            i++;
+            swap(arr, i, j);
+        }
+    }
+    swap(arr, i + 1, high);
+    return (i + 1);
+}
+  
+/* The main function that implements QuickSort
+          arr[] --> Array to be sorted,
+          low --> Starting index,
+          high --> Ending index
+ */
+function quickSort(arr, low, high) {
+    if (low < high) {
+  
+        // pi is partitioning index, arr[p]
+        // is now at right place 
+        let pi = partition(arr, low, high);
+  
+        // Separately sort elements before
+        // partition and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+  
+// Function to print an array 
+function printArray(arr, size) {
+    for (let i = 0; i < size; i++)
+        console.log(arr[i] + " ");
+  
+}
+  
+// Driver Code
+  
+let arr = [10, 7, 8, 9, 1, 5];
+let n = arr.length;
+  
+quickSort(arr, 0, n - 1);
+printArray(arr, n);
